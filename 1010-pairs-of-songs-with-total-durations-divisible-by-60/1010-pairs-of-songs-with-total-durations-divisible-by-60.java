998aecb2cdfa0e83e2997b2a3311b95bf02dcc70
Gupta126 <guptarahul126@gmail.com>
2022-10-10 22:56:40 +0530

    Time: 33 ms (24.94%), Space: 56.4 MB (23.58%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.java
@@ -4,22 +4,23 @@ class Solution {
         int mod = 60;
         
         long[] rm = new long [mod];
+        Map<Integer, Long> counter = new HashMap<Integer, Long>();
         
         long ans = 0;
         for(int i = 0; i < time.length; i++) {
-            rm[time[i] % mod]++;
+            counter.put(time[i] %mod, counter.getOrDefault((time[i] % mod), 0l) +1);
         }
         // for 0th position;
-        ans = (rm[0] * (rm[0] -1)) /2;
+        ans = (counter.getOrDefault(0, 0l) * (counter.getOrDefault(0, 0l) -1)) /2;
         
         // for the middle position 30th.  
         
-        ans = ans + (rm[mod/2] * (rm[mod/2] -1)) /2;
+        ans = ans + (counter.getOrDefault(mod/2, 0l) * (counter.getOrDefault(mod/2, 0l) -1)) /2;
         
         int i = 1, j = mod - 1;
         
         while(i < j) {
-            ans += (rm[i] * rm[j]);
+            ans += (counter.getOrDefault(i, 0l) * counter.getOrDefault(j, 0l));
             j--;
             i++;
         }
